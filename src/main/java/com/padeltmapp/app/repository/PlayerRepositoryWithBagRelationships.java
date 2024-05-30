f7ba130d220a08b24d07f9869544889c3c7f534c
@jjsanbra <jsanquis@gmail.com>
2024-05-30 17:18:25 +0200

    feat: added new entities

---
+++ src/main/java/com/padeltmapp/app/repository/PlayerRepositoryWithBagRelationships.java
@@ -0,0 +1,14 @@
+package com.padeltmapp.app.repository;
+
+import com.padeltmapp.app.domain.Player;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+
+public interface PlayerRepositoryWithBagRelationships {
+    Optional<Player> fetchBagRelationships(Optional<Player> player);
+
+    List<Player> fetchBagRelationships(List<Player> players);
+
+    Page<Player> fetchBagRelationships(Page<Player> players);
+}
