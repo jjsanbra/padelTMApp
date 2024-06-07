0fba4d5a7fedbcb73be7795d399004eecf8dc5f9
@jjsanbra <jsanquis@gmail.com>
2024-06-07 11:40:22 +0200

    feat: added tournaments section to home page

--- src/main/java/com/padeltmapp/app/config/SecurityConfiguration.java
+++ src/main/java/com/padeltmapp/app/config/SecurityConfiguration.java
@@ -69,6 +69,7 @@ public class SecurityConfiguration {
                 authz ->
                     // prettier-ignore
                 authz
+                    .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/tournaments")).permitAll()
                     .requestMatchers(mvc.pattern("/index.html"), mvc.pattern("/*.js"), mvc.pattern("/*.txt"), mvc.pattern("/*.json"), mvc.pattern("/*.map"), mvc.pattern("/*.css")).permitAll()
                     .requestMatchers(mvc.pattern("/*.ico"), mvc.pattern("/*.png"), mvc.pattern("/*.svg"), mvc.pattern("/*.webapp")).permitAll()
                     .requestMatchers(mvc.pattern("/app/**")).permitAll()
