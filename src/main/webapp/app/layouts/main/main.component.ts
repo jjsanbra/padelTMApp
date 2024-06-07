56b59ffe07fb133a22c042b9de327f3da98dafea
@jjsanbra <jsanquis@gmail.com>
2024-06-07 17:13:55 +0200

    feat: added new header component

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -1,21 +1,25 @@
-import { Component, inject, OnInit, RendererFactory2, Renderer2 } from '@angular/core';
+import { Component, inject, OnInit, RendererFactory2, Renderer2, signal } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import dayjs from 'dayjs/esm';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
+import HeaderComponent from '../header/header.component';
 import FooterComponent from '../footer/footer.component';
-import PageRibbonComponent from '../profiles/page-ribbon.component';
+import { Subject, takeUntil } from 'rxjs';
+import { Account } from 'app/core/auth/account.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'jhi-main',
   templateUrl: './main.component.html',
   providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, FooterComponent, PageRibbonComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
 })
 export default class MainComponent implements OnInit {
+  account = signal<Account | null>(null);
   private renderer: Renderer2;
 
   private router = inject(Router);
@@ -24,11 +28,17 @@ export default class MainComponent implements OnInit {
   private translateService = inject(TranslateService);
   private rootRenderer = inject(RendererFactory2);
 
+  private readonly destroy$ = new Subject<void>();
+
   constructor() {
     this.renderer = this.rootRenderer.createRenderer(document.querySelector('html'), null);
   }
 
   ngOnInit(): void {
+    this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(account => this.account.set(account));
     // try to log in automatically
     this.accountService.identity().subscribe();
 
