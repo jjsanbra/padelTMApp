7f6498f0cc7684d0da69e0455cb0651d9363f569
@jjsanbra <jsanquis@gmail.com>
2024-06-10 16:33:55 +0200

    feat: layout changes and improvements

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateDirective } from 'app/shared/language';
+import SharedModule from 'app/shared/shared.module';
 
 @Component({
   standalone: true,
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
-  imports: [TranslateDirective],
+  styleUrl: './footer.component.scss',
+  imports: [TranslateDirective, SharedModule],
 })
 export default class FooterComponent {}
